2c592dbfac4aa73c601ad08103d306cd995af879
tommyhacker <thackerstudent@gmail.com>
2021-12-13 13:18:35 +0000

    first commit

---
+++ src/components/Game.jsx
@@ -0,0 +1,177 @@
+import React, { useState, useEffect } from 'react';
+import Square from './Square';
+
+const Game = () => {
+	const combos = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	const [winner, setWinner] = useState(null);
+	const [steps, setSteps] = useState(0);
+	const calcWinner = (playerMoves) => {
+		for (let i = 0; i < combos.length; i++) {
+			const [a, b, c] = combos[i];
+			if (
+				playerMoves[a] &&
+				playerMoves[a] === playerMoves[b] &&
+				playerMoves[a] === playerMoves[c]
+			) {
+				setWinner(playerMoves[a]);
+			}
+		}
+	};
+	useEffect(() => {
+		console.log(steps);
+		if (steps === 9 && winner === null) {
+			console.log('draw game');
+			return setWinner('DRAW');
+		} else if (winner === null && steps < 9) {
+			calcWinner(moves.x);
+			calcWinner(moves.o);
+		}
+	});
+	const [moves, setMoves] = useState({
+		x: ['', '', '', '', '', '', '', '', ''],
+		o: ['', '', '', '', '', '', '', '', ''],
+	});
+	const [player, setPlayer] = useState('X');
+	const [reset, setReset] = useState(false);
+	const resetHandler = () => {
+		setReset(true);
+		setPlayer('X');
+		setMoves({
+			x: ['', '', '', '', '', '', '', '', ''],
+			o: ['', '', '', '', '', '', '', '', ''],
+		});
+		setWinner(null);
+		setSteps(0);
+	};
+
+	return (
+		<>
+			<h1 className='winner'>{winner ? `${winner}` : `${player}'s turn!`}</h1>
+			<h2 className='player-turn'>
+				{winner === 'X' || winner === 'O' ? 'WINNER!' : ``}
+			</h2>
+			<div className='game-container'>
+				<Square
+					num='0'
+					player={player}
+					setPlayer={setPlayer}
+					moves={moves}
+					setMoves={setMoves}
+					winner={winner}
+					steps={steps}
+					setSteps={setSteps}
+					reset={reset}
+					setReset={setReset}
+				/>
+				<Square
+					num='1'
+					player={player}
+					setPlayer={setPlayer}
+					moves={moves}
+					setMoves={setMoves}
+					winner={winner}
+					steps={steps}
+					setSteps={setSteps}
+					reset={reset}
+					setReset={setReset}
+				/>
+				<Square
+					num='2'
+					player={player}
+					setPlayer={setPlayer}
+					moves={moves}
+					setMoves={setMoves}
+					winner={winner}
+					steps={steps}
+					setSteps={setSteps}
+					reset={reset}
+					setReset={setReset}
+				/>
+				<Square
+					num='3'
+					player={player}
+					setPlayer={setPlayer}
+					moves={moves}
+					setMoves={setMoves}
+					winner={winner}
+					steps={steps}
+					setSteps={setSteps}
+					reset={reset}
+					setReset={setReset}
+				/>
+				<Square
+					num='4'
+					player={player}
+					setPlayer={setPlayer}
+					moves={moves}
+					setMoves={setMoves}
+					winner={winner}
+					steps={steps}
+					setSteps={setSteps}
+					reset={reset}
+					setReset={setReset}
+				/>
+				<Square
+					num='5'
+					player={player}
+					setPlayer={setPlayer}
+					moves={moves}
+					setMoves={setMoves}
+					winner={winner}
+					steps={steps}
+					setSteps={setSteps}
+					reset={reset}
+					setReset={setReset}
+				/>
+				<Square
+					num='6'
+					player={player}
+					setPlayer={setPlayer}
+					moves={moves}
+					setMoves={setMoves}
+					winner={winner}
+					steps={steps}
+					setSteps={setSteps}
+					reset={reset}
+					setReset={setReset}
+				/>
+				<Square
+					num='7'
+					player={player}
+					setPlayer={setPlayer}
+					moves={moves}
+					setMoves={setMoves}
+					winner={winner}
+					steps={steps}
+					setSteps={setSteps}
+					reset={reset}
+					setReset={setReset}
+				/>
+				<Square
+					num='8'
+					player={player}
+					setPlayer={setPlayer}
+					moves={moves}
+					setMoves={setMoves}
+					winner={winner}
+					steps={steps}
+					setSteps={setSteps}
+					reset={reset}
+					setReset={setReset}
+				/>
+			</div>
+			<button onClick={() => resetHandler()}>Reset</button>
+		</>
+	);
+};
+
+export default Game;
---
+++ src/components/Square.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+
+const Square = ({
+	num,
+	player,
+	setPlayer,
+	moves,
+	setMoves,
+	winner,
+	steps,
+	setSteps,
+	reset,
+	setReset,
+}) => {
+	useEffect(() => {
+		if (reset) {
+			setClaimed('');
+			setReset(false);
+			return;
+		} else {
+			return;
+		}
+	});
+	const [claimed, setClaimed] = useState('');
+	const clickHandler = (num) => {
+		if (winner) {
+			return;
+		} else {
+			if (claimed === '') {
+				let xMoves = [...moves.x];
+				let oMoves = [...moves.o];
+				if (player === 'X') {
+					xMoves[num] = 'X';
+					setMoves({ x: xMoves, o: oMoves });
+					setClaimed(player);
+				} else if (player === 'O') {
+					oMoves[num] = 'O';
+					setMoves({ x: xMoves, o: oMoves });
+					setClaimed(player);
+				}
+				setClaimed(player);
+				player === 'X' ? setPlayer('O') : setPlayer('X');
+				setSteps(steps + 1);
+			} else {
+				return;
+			}
+		}
+	};
+	return (
+		<div
+			className={claimed === '' ? 'square' : `square ${claimed}`}
+			onClick={() => clickHandler(num)}
+		>
+			<h1>{claimed}</h1>
+		</div>
+	);
+};
+
+export default Square;
